59acf66da738b1089dd8c3868c72a82aa1938f2f
Sashank <sashanka.samantray@rabobank.nl>
2025-07-05 01:14:38 +0200

    upgraded spring app resilience

--- spring-boot-resilience4j/src/main/java/com/circuitbreaker/resilience4j/service/BackendService.java
+++ spring-boot-resilience4j/src/main/java/com/circuitbreaker/resilience4j/service/BackendService.java
@@ -8,30 +8,20 @@ import java.util.concurrent.CompletableFuture;
 public interface BackendService {
 
     String failure();
-
     String failureWithFallback();
-
     String success();
-
     String successException();
-
     String ignoreException();
 
     Flux<String> fluxSuccess();
-
     Flux<String> fluxFailure();
-
     Flux<String> fluxTimeout();
 
     Mono<String> monoSuccess();
-
     Mono<String> monoFailure();
-
     Mono<String> monoTimeout();
 
     CompletableFuture<String> futureSuccess();
-
     CompletableFuture<String> futureFailure();
-
     CompletableFuture<String> futureTimeout();
 }
